f03ecabc56e97bf1b60f7538c38f675dea8b9972
vsqzz <jairvianen@icloud.com>
2025-06-02 02:29:44 +0200

    Update page.tsx

--- src/app/discord/page.tsx
+++ src/app/discord/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function page() {
-    return redirect("https://discord.gg/mspaint");
+    return redirect("https://discord.gg/UBaWmvbfMU");
 }
